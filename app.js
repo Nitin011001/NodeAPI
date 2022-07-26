6a8fb7695ce6e2b7da647b477ec4acba37a12b7d
Nitin011001 <thenitinchauhan01@gmail.com>
2022-07-26 09:30:08 -0400

    installed express and build the test data

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res
+    .status(404)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.....`);
+});
