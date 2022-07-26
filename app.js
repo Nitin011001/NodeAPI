14e612ca4cf26ab05b5893bc6a7ba6de1e5628d2
Nitin011001 <thenitinchauhan01@gmail.com>
2022-07-26 12:55:49 -0400

    Reponding to URL Parameter

--- app.js
+++ app.js
@@ -30,13 +30,15 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params)
+  console.log(req.params);
+
+  const id = Number(req.params.id);
+  const tour = tours.find((el) => el.id === id);
   res.status(200).json({
     status: 'success',
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
+    data: {
+      tour,
+    },
   });
 });
 
